be2df6afbfc432cfb451a4922c5378549e843768
Cristi Danciu <cristian.danciu.01@gmail.com>
2017-11-25 20:59:53 +0200

    Don't allow duplicate alarms.

--- app/redux/alarm.js
+++ app/redux/alarm.js
@@ -1,3 +1,5 @@
+import {ToastAndroid} from 'react-native'
+
 const types = {
   EDIT_ALARM: 'EDIT_ALARM',
   REMOVE_ALARM: 'REMOVE_ALARM',
@@ -39,7 +41,7 @@ const initialState = {
   alarms: [
     {hour: '10', minute: '00', label: 'Alarm 1', active: true},
     {hour: '7', minute: '09', label: 'Alarm 2', active: false},
-    {hour: '7', minute: '09', label: 'Alarm 2 Alarm 2 Alarm 2 Alarm 2 Alarm 2 Alarm 2 Alarm 2 Alarm 2 Alarm 2 Alarm 2 Alarm 2 Alarm 2', active: false}
+    {hour: '6', minute: '07', label: 'Alarm 2 Alarm 2 Alarm 2 Alarm 2 Alarm 2 Alarm 2 Alarm 2 Alarm 2 Alarm 2 Alarm 2 Alarm 2 Alarm 2', active: false}
   ],
   editIndex: -1,
   showAddDialog: false
@@ -51,21 +53,7 @@ export const reducer = (state=initialState, action) => {
 
   switch(type) {
     case types.EDIT_ALARM: {
-      let index = editIndex > -1 ? editIndex : alarms.length
-
-      alarms[index] = {
-        hour: payload.hour,
-        minute: payload.minute,
-        label: payload.label,
-        active: true
-      }
-
-      return {
-        ...state,
-        showAddDialog: false,
-        editIndex: -1,
-        alarms: alarms
-      }
+      return _editAlarm(state, payload)
     }
 
     case types.REMOVE_ALARM: {
@@ -106,5 +94,38 @@ export const reducer = (state=initialState, action) => {
     default: {
       return state
     }
+  }
+}
+
+_editAlarm = (state, payload) => {
+  const {alarms, editIndex} = state
+  const index = editIndex > -1 ? editIndex : alarms.length
+
+  const existingAlarms = alarms.filter((alarm, i) => {
+    return i != index
+      && alarm.hour == payload.hour
+      && alarm.minute == payload.minute
+  })
+
+  if (existingAlarms.length) {
+    ToastAndroid.show(
+      `An alarm set for ${payload.hour}:${payload.minute} already exist!`,
+      ToastAndroid.SHORT
+    )
+    return state
+  }
+
+  alarms[index] = {
+    hour: payload.hour,
+    minute: payload.minute,
+    label: payload.label,
+    active: true
+  }
+
+  return {
+    ...state,
+    showAddDialog: false,
+    editIndex: -1,
+    alarms: alarms
   }
 }
